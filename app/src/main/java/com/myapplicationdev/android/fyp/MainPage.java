5937e2b27b26fa874d59743431bec7b515c756a1
19023980 <19023980@myrp.edu.sg>
2021-05-30 15:27:15 +0800

    minor commit

--- app/src/main/java/com/myapplicationdev/android/fyp/MainPage.java
+++ app/src/main/java/com/myapplicationdev/android/fyp/MainPage.java
@@ -2,13 +2,29 @@ package com.myapplicationdev.android.fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainPage extends AppCompatActivity {
-
+    Button btnStart,btnRevision,btnHowToPlay,btnSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btnStart);
+        btnHowToPlay = findViewById(R.id.btnHowToPlay);
+        btnRevision = findViewById(R.id.btnRevisionSection);
+        btnSettings = findViewById(R.id.btnSettings);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainPage.this,Difficulty_Section.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
