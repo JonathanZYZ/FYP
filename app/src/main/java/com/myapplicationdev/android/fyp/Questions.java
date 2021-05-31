73dae0b4e64c2e3435432a93dc5421ccb99604e9
zuhaili <19004781@myrp.edu.sg>
2021-05-31 20:34:20 +0800

    zuhaili

--- app/src/main/java/com/myapplicationdev/android/fyp/Questions.java
+++ app/src/main/java/com/myapplicationdev/android/fyp/Questions.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Question2_Easy extends AppCompatActivity {
+public class Questions extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question2_easy);
+        setContentView(R.layout.activity_questions);
     }
 }
