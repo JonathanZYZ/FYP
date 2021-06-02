6d6bbf9ae48ed861df0d752f25a59c8a78080567
zuhaili <19004781@myrp.edu.sg>
2021-06-02 10:37:50 +0800

    XML files updated

---
+++ app/src/main/java/com/myapplicationdev/android/fyp/Answered_Incorrectly.java
@@ -0,0 +1,14 @@
+package com.myapplicationdev.android.fyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Answered_Incorrectly extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answered_incorrectly);
+    }
+}
