cd182ad35e761625e41f7ffb85a99cb4427dc780
JonathanZYZ <jonathanzengyuzhao@gmail.com>
2021-06-02 10:11:47 +0800

    Test

--- app/src/main/java/com/myapplicationdev/android/fyp/Questions.java
+++ app/src/main/java/com/myapplicationdev/android/fyp/Questions.java
@@ -2,13 +2,27 @@ package com.myapplicationdev.android.fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Questions extends AppCompatActivity {
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
+        btnStart = findViewById(R.id.btnStart)
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
     }
 }
