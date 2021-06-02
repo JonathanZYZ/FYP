6d34860c5e0f81f49e52d26f57483db7de592d0e
zuhaili <19004781@myrp.edu.sg>
2021-06-02 10:53:24 +0800

    XML files updated

---
+++ app/src/main/java/com/myapplicationdev/android/fyp/Question_Correct.java
@@ -0,0 +1,14 @@
+package com.myapplicationdev.android.fyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Question_Correct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_correct);
+    }
+}
