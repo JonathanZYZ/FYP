2a9f1d8cb600f53c50df00796b6857844de812f1
JonathanZYZ <jonathanzengyuzhao@gmail.com>
2021-06-24 09:14:33 +0800

    Test

--- app/src/main/java/com/myapplicationdev/android/fyp/SettingPageActivity.java
+++ app/src/main/java/com/myapplicationdev/android/fyp/SettingPageActivity.java
@@ -2,13 +2,37 @@ package com.myapplicationdev.android.fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class SettingPageActivity extends AppCompatActivity {
+
+    Button btnApply;
+    CheckBox checkBoxSound, checkBoxMusic;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_page);
+
+        btnApply = findViewById(R.id.btnApply);
+        checkBoxSound = findViewById(R.id.checkBoxSound);
+        checkBoxMusic = findViewById(R.id.checkBoxMusic);
+
+        btnApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkBoxSound.isChecked()){
+                    //store in database
+                }
+                if(checkBoxMusic.isChecked()){
+                    //store in database
+                }
+            }
+        });
     }
 }
