d6e54d053cd0e6925ceed3da5579542e4eb11135
JonathanZYZ <jonathanzengyuzhao@gmail.com>
2021-07-08 15:07:18 +0800

    Test

---
+++ app/src/main/java/com/myapplicationdev/android/fyp/AudioData.java
@@ -0,0 +1,37 @@
+package com.myapplicationdev.android.fyp;
+
+public class AudioData {
+    // static variable single_instance of type Singleton
+    private static AudioData single_instance = null;
+
+    // variable of type String
+    private boolean isEnabledMusic;
+    private boolean isEnabledSound;
+
+    // private constructor restricted to this class itself
+    private AudioData() { }
+
+    // static method to create instance of Singleton class
+    public static AudioData getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new AudioData();
+        return single_instance;
+    }
+
+    public boolean isEnabledMusic() {
+        return isEnabledMusic;
+    }
+
+    public void setEnabledMusic(boolean enabledMusic) {
+        isEnabledMusic = enabledMusic;
+    }
+
+    public boolean isEnabledSound() {
+        return isEnabledSound;
+    }
+
+    public void setEnabledSound(boolean enabledSound) {
+        isEnabledSound = enabledSound;
+    }
+}
