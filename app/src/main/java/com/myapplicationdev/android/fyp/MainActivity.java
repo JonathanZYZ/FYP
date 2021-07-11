8ec482f9f9212f294e47bd89bee353df5d32dcf4
19023980 <19023980@myrp.edu.sg>
2021-07-11 19:20:05 +0800

    some bugs fixed

--- app/src/main/java/com/myapplicationdev/android/fyp/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/fyp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Button;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
         btnStart = findViewById(R.id.Play);
@@ -80,11 +82,13 @@ public class MainActivity extends AppCompatActivity {
         Boolean music = sharedPreferences.getBoolean("music",true);
         if(music==true){
             backgroundMusic = MediaPlayer.create(MainActivity.this,R.raw.background_music);
-            backgroundMusic.start();
         } else{
             backgroundMusic = new MediaPlayer();
-            backgroundMusic.start();
         }
+        if (backgroundMusic.isPlaying()){
+            backgroundMusic.stop();
+        }
+
     }
 
     @Override
